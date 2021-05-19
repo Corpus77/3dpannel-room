d737a6a0b0e3c943ad36f0972b6c0ecf908b2cd6
Corpus77 <corpusne@gmail.com>
2021-05-20 02:21:40 +0300

    final logic commit

--- roombrick/index2.js
+++ roombrick/index2.js
@@ -1,14 +1,14 @@
-let brick = document.createElement('div');
-brick.className = 'brick';
+
 let wall = document.querySelector('.wall');
 let inWall = document.querySelector('#inwall');
+let clearAll = document.querySelector(".clearAll");
 
 //add bricks to the wall
 for (let i = 0;i<400;i++) {
     let brick = document.createElement('div');
     brick.className = 'brick';
     wall.appendChild(brick);
-    // console.log(i);
+    
 }
 //move parity rows
 let row = 0;
@@ -16,17 +16,23 @@ for (let i = 0; i<400;i++) {
     if (i%32 == 0) {
         for (let row = 0; row<16; row++){
             wall.children[i+row].style.left = 3 + '%';
-            // console.log(i);
+            
         }
     }
-} 
+}
+let brickList = document.querySelectorAll(".brick") ;
 wall.addEventListener ('click', paint);
 wall.addEventListener ('dblclick', clear);
+clearAll.onclick = (e) => {
+   brickList.forEach((item) => {
+       item.style.backgroundImage = "none";
+   }) 
+}
 function clear(event) {
     if (event.target.classList.contains('brick')) {
         event.target.style.backgroundImage = 
         `none`;
-        console.log('clear')
+        
     }
 }
 function paint (event) {
@@ -35,7 +41,7 @@ if (inWall.files[0].name) {
         event.target.style.backgroundImage = 
         `url(../textures/${inWall.files[0].name})`;
     }
-    console.log(inWall.files[0].name)
+    
 }
     
     
